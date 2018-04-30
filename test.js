50c9a2930297dab8fd0c163857bceed16a2410c3
David Waller <david@krawaller.se>
2018-04-30 12:31:21 +0200

    make test use multiple args

--- test.js
+++ test.js
@@ -9,21 +9,21 @@ test('it emits function return values until unsub', t => {
   const report = (name,dir,t,d) => t === 1 && received.push(d);
   const sink = makeMock('sink', report);
 
-  const identity = a => a;
-  const {source, emitter} = fromFunction(identity);
+  const logic = (...args) => args.join('-');
+  const {source, emitter} = fromFunction(logic);
 
   emitter('foo'); // won't be received since we're not subscribed yet
 
   source(0, sink); // start subscription
 
-  emitter('bar');
-  emitter('baz');
+  emitter('bar', 456);
+  emitter('baz', 7, 8, 9);
 
   sink.emit(2); // stop subscription
 
   emitter('bin'); // won't be received since we stopped subscribing
 
-  t.deepEqual(received, ['bar', 'baz'], 'all return values are emitted');
+  t.deepEqual(received, ['bar-456', 'baz-7-8-9'], 'all return values are emitted');
 
   t.end();
 });
